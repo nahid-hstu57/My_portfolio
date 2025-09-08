a20f622a95aa500c43d8cb0a9cd77bf7992de41f
MD Nahid Hasan <hmdnahid321@gmail.com>
2025-09-08 17:52:21 +0600

    first commit

---
+++ my_portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl align-center bg-red-500'>Hellow world!</h1>
+    </>
+  )
+}
+
+export default App
